3792a3eac8d8f5c062b48704c7c9d75695d7f103
zedin27 <zeidtisnes@gmail.com>
2018-07-25 22:11:45 -0700

    finally it works lmao

--- hello_proper_exit.asm
+++ hello_proper_exit.asm
@@ -1,16 +1,16 @@
 section .data
-message: db 'hello, world!', 10
+	message: db 'hello, world!', 10
 
 section .text
-global _start
+	global start
 
-_start:
-	mov		rax, 1
+start:
+	mov		rax, 0x2000004
 	mov		rdi, 1
 	mov		rsi, message
 	mov		rdx, 14
 	syscall
 
-	mov		rax, 60
+	mov		rax, 0x2000001
 	xor		rdi, rdi
 	syscall
