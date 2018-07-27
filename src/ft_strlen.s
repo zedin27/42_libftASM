c07cb0e48fcdb5fe2d772a924ec40c1da60c6488
zedin27 <ztisneshamzeh@csumb.edu>
2018-07-27 14:17:48 -0700

    lord and savior: tony

--- src/ft_strlen.s
+++ src/ft_strlen.s
@@ -7,9 +7,9 @@ global ft_strlen						; main
 ft_strlen:
 	xor		rax, rax
 	.loop:
-		cmp byte [rdi + rax], 0				; compares null character to the byte
-		je .end								; if is NULL, then end
-		inc rax								; increase rax by 1
-		jmp .loop							; jumps to the next iteration
+		cmp byte [rdi + rax], 0			; compares null character to the byte
+		je .end							; if is NULL, then end
+		inc rax							; increase rax by 1
+		jmp .loop						; jumps to the next iteration
 	.end:
 		ret
