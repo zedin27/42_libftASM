1963bd20d9ee75a6028de090f6a7bc15d913f6b1
zedin27 <ztisneshamzeh@csumb.edu>
2018-07-26 22:26:53 -0700

    need to find a problem in strlen?

--- src/ft_strlen.s
+++ src/ft_strlen.s
@@ -1,28 +1,28 @@
 [bits 64]
 
-global start
-
 section .data
-	strn: db "hello", 0
-
+	strn: db "hello", 0					; "hello" to NUL-byte
 section .text
+global start							; main
 
 ft_strlen:
-	xor rax, rax
+	xor rax, rax						; rax will store the length of strn
 
 .loop:
-	cmp byte [rdi + rax], 0
-	je .end
-	inc rax
-	jmp .loop
+	cmp byte [rdi + rax], 0				; compares next character a null character
+	je .end								; if is NULL, then .end
+	inc rax								; increase rax by 1
+	jmp .loop							; jumps to the next iteration
 
 .end:
 	ret
 
 start:
+	mov rax, 0x2000004
 	mov rdi, strn
 	call ft_strlen
 	mov rdi, rax
 
-	mov rax, 60
+	;exit
+	mov rax, 0x2000001
 	syscall
