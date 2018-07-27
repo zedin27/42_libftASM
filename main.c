dec897bac6cfcd240cfae369eb99fa7b8a7c81f3
zedin27 <ztisneshamzeh@csumb.edu>
2018-07-26 22:27:29 -0700

    added ft_strlen(void) for now

--- main.c
+++ main.c
@@ -1,8 +1,12 @@
 #include <unistd.h>
+#include <stdio.h>
 
-extern int ft_hello(void);
+extern	int ft_hello(void);
+extern	unsigned ft_strlen(void);
 
 int		main(void)
 {
+	char test[6] = "abcde\0";
 	ft_hello();
+	printf("strlen is: %d\n", ft_strlen());
 }
