88f53e922762d64123632061d9515db6b2c18b45
zedin27 <ztisneshamzeh@csumb.edu>
2018-07-26 22:27:13 -0700

    added few comments

--- hello_proper_exit.s
+++ hello_proper_exit.s
@@ -3,22 +3,19 @@
 [bits 64]
 
 section .data
-	message: db 'hello, world!', 10
+	message: db 'hello, world!', 10	; the number '10' represents newline in ASCII
+									; 'hello, world!' to newline basically
 section .text
 global start
-global exit
 
 start:
-	mov		rax, 0x2000004
-	mov		rdi, 0x01
-	mov		rsi, message
-	mov		rdx, 14
-	syscall
+	mov		rax, 0x2000004				; holds the syscall # write(rdi, rsi, rdx)
+	mov		rdi, 0x01					; first file descriptor (stdout)
+	mov		rsi, message				; read string 'hello, world!'
+	mov		rdx, 14						; setting buff size to 14
+	syscall								; call kernel
 
-	mov		rax, 0x2000001
-	xor		rdi, rdi
-	syscall
-
-exit:
-	mov		rax, 60
+	; exit
+	mov		rax, 0x2000001				; exit system call hex number
+	xor		rdi, rdi					; exit status
 	syscall
