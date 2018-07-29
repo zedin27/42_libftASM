a623a69a969ebab95304459457fc08be0d39dcc0
zedin27 <ztisneshamzeh@csumb.edu>
2018-07-28 22:16:33 -0700

    added bzero, strlen, and puts

--- main.c
+++ main.c
@@ -1,14 +1,21 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <string.h>
 
-extern	int ft_hello(void);
-extern	unsigned ft_strlen(char *str);
+extern	int			ft_hello(void);
+extern	unsigned	ft_strlen(char *str);
+extern	unsigned	ft_bzero(void *s, size_t n);
+extern	void		ft_puts(char *str);
 
 int		main(void)
 {
 	char str[3] = "abc";
 
 	//ft_hello();
-	printf("strlen is: %d\n", ft_strlen(str));
+	printf("ft_strlen is: %d\n", ft_strlen(str));
+	printf("OG strlen is: %d\n\n", ft_strlen(str));
+
+	ft_bzero(str, 3);
+	ft_puts("hello");
 	return (0);
 }
