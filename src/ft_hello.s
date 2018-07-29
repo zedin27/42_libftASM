3abe14611774e7929c26bf33ba9c6e085fb4ee97
zedin27 <ztisneshamzeh@csumb.edu>
2018-07-28 18:56:11 -0700

    implementing macros

--- src/ft_hello.s
+++ src/ft_hello.s
@@ -8,12 +8,12 @@ section .data
 section .text
 
 _ft_hello:
-	mov		rax, 0x2000004		; syscall write
+	mov		rax, SYS_WRITE		; syscall write
 	mov		rdi, 1				; stdout fd (where will it write?)
 	mov		rsi, hello_world	; string address (where does it start?)
 	mov		rdx, 20				; string length in bytes (how many bytes to write?)
 	syscall						; system call
 
-	mov		rax, 0x2000001		; exit system call
+	mov		rax, SYS_EXIT		; exit system call
 	xor		rdi, 0				; 0 can be replaced with rdi
 	syscall
