af058325f6e152f7b18103a1db6ce6dfd434c6f3
zedin27 <ztisneshamzeh@csumb.edu>
2018-07-28 22:15:36 -0700

    revise more

--- src/ft_bzero.s
+++ src/ft_bzero.s
@@ -9,3 +9,15 @@ section .text
 
 ; unsigned ft_bzero(void *s, size_t n)
 _ft_bzero:
+	xor		rax, rax
+	.start:
+		cmp		rsi, rax				; compare rax to the src (rax==0)
+		jz		.exit					; exit if the compare is zero
+
+	dec		rsi							; decrease src
+	mov		byte [rdi + rsi], 0			; sets '\0' to the memory of dst[src]
+	jmp		.start						; once it finishes, jump to the label
+										; .start
+
+	.exit:
+		ret
