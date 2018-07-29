769ec254f704b009445438d23549db4d94e46962
zedin27 <ztisneshamzeh@csumb.edu>
2018-07-28 22:15:19 -0700

    ft_strlen

--- src/ft_strlen.s
+++ src/ft_strlen.s
@@ -7,11 +7,11 @@ global ft_strlen						; main
 ft_strlen:
 	push	rdi				; pushing stack to file descriptor
 	sub		rcx, rcx		;
-	cmp byte [rdi + rax], 0	; compares null character to the byte
+	cmp byte [rdi + rax], 0	; compares null character to the byte, so dst[rax]
 	not		rcx				; two's complement
-	sub		rax, rax		; setting up to NULL
+	sub		rax, rax		; setting up to NULL by subtracting
 
-	cld
+	cld						; clear data
 	repnz	scasb			; scan string for NUL, decrementing each char
 	not		rcx				; again, reversing all bits
 	dec		rcx				; decrement rcx by one, which will contain then
