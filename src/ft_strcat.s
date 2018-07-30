ba17b2e6ff9d09d4d459f918ba9361aee5d05687
zedin27 <ztisneshamzeh@csumb.edu>
2018-07-30 07:41:31 -0700

    ft_strcat ok

--- src/ft_strcat.s
+++ src/ft_strcat.s
@@ -1,25 +1,30 @@
-section .text
+[bits 64]
+
+extern _ft_strlen							; use this future reference
 global	_ft_strcat
 
+section .text
+
+; char *ft_strcat(char *s1, const char *s2)
 _ft_strcat:
 	mov		rbx, rdi
 	mov		rcx, rsi
 
-loop1:
-	cmp		byte [rbx], 0
-	je		loop2
-	inc		rbx
-	jmp		loop1
+	.loop1:
+		cmp		byte [rbx], 0
+			je		.loop2
+		inc		rbx
+			jmp		.loop1
 
-loop2:
-	cmp		byte [rcx], 0
-	je		end
-	mov		al, byte [rcx]
-	mov		byte [rbx], al
-	inc		rcx
-	inc		rbx
-	jmp		loop2
+	.loop2:
+		cmp		byte [rcx], 0		; if rcx[rsi] NULL, then exit
+			je		.exit
+		mov		al, byte [rcx]		; add logic of the rcx byte offset
+		mov		byte [rbx], al		; use that add logical value into rbx byte
+		inc		rcx					; rcx++
+		inc		rbx					; rbx++
+			jmp		.loop2			; repeat to loop2
 
-end:
-	mov		rax, rdi
-	ret
+	.exit:
+		mov		rax, rdi
+		ret
