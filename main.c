9907cbce8e92d8d652208604342507cea41cca4c
zedin27 <ztisneshamzeh@csumb.edu>
2018-07-29 17:38:40 -0700

    testing isalpha

--- main.c
+++ main.c
@@ -3,19 +3,29 @@
 #include <string.h>
 
 extern	int			ft_hello(void);
-extern	unsigned	ft_strlen(char *str);
+extern	unsigned	ft_strlen(const char *str);
 extern	unsigned	ft_bzero(void *s, size_t n);
 extern	void		ft_puts(char *str);
+extern	int			ft_isalpha(int c);
 
 int		main(void)
 {
 	char str[3] = "abc";
 
 	//ft_hello();
-	printf("ft_strlen is: %d\n", ft_strlen(str));
-	printf("OG strlen is: %d\n\n", ft_strlen(str));
+	//printf("ft_strlen is: %d\n", ft_strlen(str)); //testing ft_strlen.s
+	//printf("OG strlen is: %d\n\n", ft_strlen(str));
 
-	ft_bzero(str, 3);
-	ft_puts("hello");
+	//ft_bzero(str, 3); //testing ft_bzero
+	//ft_puts("hello"); //testing ft_puts
+	printf("%d\n", ft_isalpha('a')); //1
+	printf("%d\n", ft_isalpha('z')); //1
+	printf("%d\n", ft_isalpha('A')); //1
+	printf("%d\n\n", ft_isalpha('Z')); //1
+
+	printf("%d\n", ft_isalpha(125)); //0
+	printf("%d\n", ft_isalpha(50)); //0
+	printf("%d\n", ft_isalpha(133)); //0
+	printf("%d\n", ft_isalpha(-1)); //0
 	return (0);
 }
