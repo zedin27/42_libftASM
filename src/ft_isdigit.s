d8177c412b4c20b9f1ac3d26dcc8e05933b0c9b0
zedin27 <zeidtisnes@gmail.com>
2018-07-29 20:14:41 -0700

    works

---
+++ src/ft_isdigit.s
@@ -0,0 +1,32 @@
+[bits 64]
+
+section .text
+global _ft_isdigit
+
+; int		_ft_isdigit(int c)
+_ft_isdigit:
+	xor		rax, rax ; rax is zero at the moment
+
+	cmp		rdi, 0x30 ; (c < 0)
+	jl		.false
+
+	cmp		rdi, 0x39 ; (9 > c)
+	jg		.false
+
+	.true:
+		mov	rax, 1
+	.false:
+		ret
+
+; int		ft_isdigit(int c)
+; {
+;	if (c >= '0' && c <= '9')
+;		return (1);
+;	return (0);
+; }
+
+
+; jle (<=)
+; jge (>=)
+; jl (<)
+; jg (>)
