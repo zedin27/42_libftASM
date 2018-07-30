82afb106acf05c14a2694f99cc1feb891d41d743
zedin27 <ztisneshamzeh@csumb.edu>
2018-07-30 07:25:04 -0700

    ft_cat done

---
+++ src/ft_cat.s
@@ -0,0 +1,40 @@
+[bits 64]
+
+global _ft_cat
+%define SYS_READ 0x2000003
+%define SYS_WRITE 0x2000004
+%define STDOUT 0x01
+%define BUFF_SIZE 0xff
+
+section			.bss
+	buffer		resb		BUFF_SIZE		; unitialised storage space, basically reserving BUFF_SIZE bytes
+
+section			.text
+
+; extern	int				ft_cat(int fd);
+_ft_cat:
+	xor		rax, rax
+	push	rbp
+	mov		rbp, rsp
+
+	.read:
+		push	rdi					; push rdi stack first before we start reading
+		mov		rsi, buffer
+		mov		rdx, BUFF_SIZE
+		mov		rax, SYS_READ 		; read
+		syscall
+			jc		end				; jump carry
+		cmp		rax, 0
+			jle		end
+	.write:
+		mov		rdi, STDOUT			; output fd
+		mov		rdx, rax			; store the destination of rax
+		mov		rax, SYS_WRITE		; write
+		syscall
+		pop		rdi					; take out our initial rdi stack
+			jmp		.read			; read again
+
+	end:
+		mov		rsp, rbp
+		pop		rbp
+		ret
