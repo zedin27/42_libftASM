75ac9ff1aedebe615871620a1424efdb478e6c75
zedin27 <zeidtisnes@gmail.com>
2018-07-29 20:23:55 -0700

    works

---
+++ src/ft_isprint.s
@@ -0,0 +1,31 @@
+[bits 64]
+
+section .text
+global _ft_isprint
+
+; int		_ft_isprint(int c)
+_ft_isprint:
+	xor		rax, rax ; rax is zero at the moment
+
+	cmp		rdi, 0x20 ; (c < ' ')
+	jl		.false
+
+	cmp		rdi, 0x7e ; ('~' > c)
+	jg		.false
+
+	.true:
+		mov	rax, 1
+	.false:
+		ret
+
+; int		ft_isprint(int c)
+; {
+;	if (c >= ' ' && c <= '~')
+;		return (1);
+;	return (0);
+; }
+
+; jle (<=)
+; jge (>=)
+; jl (<)
+; jg (>)
