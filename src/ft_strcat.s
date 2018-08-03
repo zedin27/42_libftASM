aab5dc4dcf84c53b88a82c95f3a9a59e1071501c
zedin27 <ztisneshamzeh@csumb.edu>
2018-08-02 17:20:28 -0700

    nothing changed

--- src/ft_strcat.s
+++ src/ft_strcat.s
@@ -11,19 +11,19 @@ _ft_strcat:
 	mov		rcx, rsi
 
 	.loop1:
-		cmp		byte [rbx], 0
-			je		.loop2
-		inc		rbx
-			jmp		.loop1
+		cmp		byte [rbx], 0		; if my rbx value is NULL
+		je		.loop2				; jump to the second loop
+		inc		rbx					; otherwise increase the rbx ptr
+		jmp		.loop1				; repeat
 
 	.loop2:
 		cmp		byte [rcx], 0		; if rcx[rsi] NULL, then exit
-			je		.exit
+		je		.exit
 		mov		al, byte [rcx]		; add logic of the rcx byte offset
 		mov		byte [rbx], al		; use that add logical value into rbx byte
 		inc		rcx					; rcx++
 		inc		rbx					; rbx++
-			jmp		.loop2			; repeat to loop2
+		jmp		.loop2				; repeat to loop2
 
 	.exit:
 		mov		rax, rdi
