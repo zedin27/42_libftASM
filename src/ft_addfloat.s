1a7957fd7123119b54476d45930208d951e7b4b1
zedin27 <ztisneshamzeh@csumb.edu>
2018-08-06 22:56:16 -0700

    wrong instruction

--- src/ft_addfloat.s
+++ src/ft_addfloat.s
@@ -3,11 +3,11 @@
 section .text
 global _ft_addfloat
 
-; void ft_addfloat(float[], float[])
+; float ft_addfloat(float[], float[])
 
 _ft_addfloat:
 	movdqa	xmm0, [rdi]
 	movdqa	xmm1, [rsi]
-	addpd	xmm0, xmm1
+	addps	xmm0, xmm1
 	movdqa	[rdi], xmm0
 	ret
