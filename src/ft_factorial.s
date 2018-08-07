c31aa2eacfb698b958b9e3dc51326b14b62c0b13
zedin27 <ztisneshamzeh@csumb.edu>
2018-08-06 22:06:03 -0700

    factorial function

---
+++ src/ft_factorial.s
@@ -0,0 +1,20 @@
+[bits 64]
+
+section .text
+global _ft_factorial
+
+; uint64_t ft_factorial(uint64_t num)
+
+_ft_factorial:
+	cmp		rdi, 1
+	jnbe	.rec
+	mov		rax, 1
+	ret
+
+.rec:
+	push	rdi
+	dec		rdi
+	call	_ft_factorial
+	pop		rdi
+	imul		rax, rdi
+	ret
