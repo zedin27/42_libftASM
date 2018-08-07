f6007cf4e3597c10cc61caf50563c4fc4caad6ac
zedin27 <ztisneshamzeh@csumb.edu>
2018-08-06 22:05:49 -0700

    5hrs for this lmao

--- src/ft_divide.s
+++ src/ft_divide.s
@@ -1,15 +1,15 @@
-global _ft_divide
+; [bits 64]
 
+global _ft_divide
 section .text
 
-; int ft_divide(int dividend, int divisor)
+; int32_t ft_divide(int32_t dividend, int32_t divisor)
 _ft_divide:
+	mov		eax, edi		; Get 32-bit dividend and
+	cdq						; sign extend to 64-bit edx:eax
 
-	mov rdx, 0
-	mov rax, rdi		; dividend
-	mov rcx, rsi		; divisor
-	div rcx
-	push rbx
+	idiv	esi				; divide
 
-	add rsp , 8
-	ret
+	and		rax, -1			; Force upper bits to zero,
+							; probably not required, but nice.
+	ret                     ; Return quotient in rax/eax.
