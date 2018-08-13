ac3aa52eead69f90e978c41bac19358c60ab99c7
zedin27 <ztisneshamzeh@csumb.edu>
2018-08-12 19:52:52 -0700

    currently getting fixed

--- src/ft_sqrt.s
+++ src/ft_sqrt.s
@@ -5,7 +5,7 @@ global _ft_sqrt
 
 ; double sqrt(double num);
 _ft_sqrt:
-	fld		qword [rdi]		; read the number given
-	fsqrt
-	fst		qword [rax]
-	ret
+	fld		qword [rdi]		; float load or read the number given
+	fsqrt					; float square root instruction
+	fst		qword [rax]		; float store value to rax
+	ret						; and return it's value
