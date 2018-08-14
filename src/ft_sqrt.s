c402cd4ab225c35e510caabcbfa0cafa7aeac683
zedin27 <ztisneshamzeh@csumb.edu>
2018-08-13 18:20:31 -0700

    no need to return rax

--- src/ft_sqrt.s
+++ src/ft_sqrt.s
@@ -5,7 +5,5 @@ global _ft_sqrt
 
 ; double sqrt(double num);
 _ft_sqrt:
-	fld		qword [rdi]		; float load or read the number given
-	fsqrt					; float square root instruction
-	fst		qword [rax]		; float store value to rax
+	sqrtpd	xmm0, xmm0
 	ret						; and return it's value
